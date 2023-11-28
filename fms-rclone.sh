484ee19e5d0cc4a0917ea020f0d5b39b27f8027b
Mohamed Tawfiq <mhtawfiq@gmail.com>
2023-11-28 21:21:45 +0300

    Update fms-rclone.sh

--- fms-rclone.sh
+++ fms-rclone.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# FileMaker Server Off-site Backup using rclone
+# Claris FileMaker Server Off-site Backup using rclone
 # By Mohamed H. Tawfiq - mhtawfiq@gmail.com
 # 20231128 - Initial Release
 
@@ -9,6 +9,9 @@ fmsbackupsf="/opt/FileMaker/FileMaker Server/Data/Backups"
 # rclone Remote
 rcr="fms-backup"
 
+# S3 Bucket Name
+s3="fms-backup1"
+
 # Retention Policy
 rdaily=90
 rweekly=24
@@ -16,10 +19,10 @@ rmonthly=36
 
 # Day of the Week for Weekly Backups
 # Monday (1) to Sunday (7)
-dweekly=7
+dweekly=2
 
 # Day of the Month for Monthly Backups
-dmonthly=1
+dmonthly=28
 
 
 ###
@@ -27,27 +30,33 @@ dmonthly=1
 ###
 
 
+# Latest Folder
+cd "${fmsbackupsf}"
+latestf=$(ls -td -- */ | head -n 1 | cut -d'/' -f1)
+
 # FileMaker Server Default Backups Day Timestamp
 fmday=$(date +%Y-%m-%d)
 
 # rclone Copy Backups to Remote
 
-# Daily 
-rclone copy --create-empty-src-dirs ${fmsbackupsf}/*_${fmday}_* ${rcr}:fms-backup/daily
+echo "${fmsbackupsf}/${latestf}" ${rcr}:${s3}/daily/${latestf}
+
+# Daily
+rclone copy "${fmsbackupsf}/${latestf}" ${rcr}:${s3}/daily/${latestf}
 
 # Weekly
 if [ $(date +%u) -eq $dweekly ]
 then
-	rclone copy --create-empty-src-dirs ${fmsbackupsf}/*_${fmday}_* ${rcr}:fms-backup/weekly
+        rclone copy "${fmsbackupsf}/${latestf}" ${rcr}:${s3}/weekly/${latestf}
 fi
 
 # Monthly
 if [ $(date +%d) -eq $dmonthly ]
 then
-	rclone copy --create-empty-src-dirs ${fmsbackupsf}/*_${fmday}_*/ ${rcr}:fms-backup/monthly
+        rclone copy "${fmsbackupsf}/${latestf}" ${rcr}:${s3}/monthly/${latestf}
 fi
 
 # Clean Remote
-rclone delete --min-age ${rdaily}d ${rcr}:fms-backup/daily
-rclone delete --min-age ${rweekly}w ${rcr}:fms-backup/daily
-rclone delete --min-age ${rmonthly}M ${rcr}:fms-backup/weekly
+rclone delete --rmdirs --min-age ${rdaily}d ${rcr}:${s3}/daily
+rclone delete --rmdirs --min-age ${rweekly}w ${rcr}:${s3}/weekly
+rclone delete --rmdirs --min-age ${rmonthly}M ${rcr}:${s3}/monthly
